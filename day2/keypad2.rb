2ac65d57ef9be94a6b904b631b89a568701ad347
Cody McCarthy <cody@colibri-software.com>
2016-12-08 22:33:33 -0400

    Finish part 2 of day 1

--- day2/keypad2.rb
+++ day2/keypad2.rb
@@ -1,18 +1,20 @@
 require 'csv'
-keypad = [[1,2,3],[4,5,6],[7,8,9]]
+keypad = [[0,0,1,0,0],[0,2,3,4,0],[5,6,7,8,9],[0,'A','B','C',0],[0,0,'D',0,0]]
 input_file = './input.csv'
-position = {x: 1, y: 1}
+position = {x: 0, y: 2}
 code = []
 CSV.foreach(input_file) do |row|
   row.each do |str|
     (0..str.length - 1).each do |i|
+      previous_x = position[:x]
+      previous_y = position[:y]
       case str[i]
       when 'L'
         if position[:x] > 0
           position[:x] -= 1
         end
       when 'R'
-        if position[:x] < 2
+        if position[:x] < keypad[position[:y]].length - 1
           position[:x] += 1
         end
       when 'U'
@@ -20,10 +22,14 @@ CSV.foreach(input_file) do |row|
           position[:y] -= 1
         end
       when 'D'
-        if position[:y] < 2
+        if position[:y] < 4
           position[:y] += 1
         end
       end
+     if keypad[position[:y]][position[:x]] == 0
+       position[:x] = previous_x
+       position[:y] = previous_y
+     end
     end
     code << keypad[position[:y]][position[:x]]
   end
